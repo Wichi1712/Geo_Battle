d0d39c7e7b3aa739c533b424af604e8c6fd788de
Wichi1712 <willysag@gmail.com>
2019-09-14 00:57:04 -0500

    nueva

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,8 @@
 var Menu = {
 
 preload: function() {
-  juego.stage.backgroundColor = "#FFF";  
+  juego.stage.backgroundColor = "#FFF";
+  juego.load.image("triangle", "image/triangulo_0.png");
 },
 
 
