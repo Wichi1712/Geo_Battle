7e2e9aa7ebb8dbd5656f9dd2c4be944e94d8ca8a
wichi1712 <willysag@gmail.com>
2024-09-26 20:11:26 -0500

    26/09/2024

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,6 @@
 var Menu = {
-
-preload: function() {
-  juego.stage.backgroundColor = "#FFF";
-  juego.load.image("triangle", "image/triangulo_0.png");
-},
-
-
-
-
-
-}
-
-
-
-
-
+  preload: function () {
+    juego.stage.backgroundColor = "#FFF";
+    juego.load.image("triangle", "image/triangulo_0.png");
+  },
+};
